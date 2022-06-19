e9fa43306c2c6a62938e61b832960072b2053460
Eduardo de Oliveira Henrique <dudu.sji06@gmail.com>
2022-06-19 20:16:15 -0300

    Second Commit

--- script/script.js
+++ script/script.js
@@ -1,8 +1,33 @@
-$.ajax({
-  type: 'GET',
-  url: "https://valorant-api.com/v1/weapons",
-  data: "",
-  success: function (data) {
-    console.log(data);
-  },
+var arrayReturn = {};
+var number = 0;
+var category = null;
+if (arrayReturn != " ") {
+  $.ajax({
+   type: 'GET',
+   url: "https://valorant-api.com/v1/weapons",
+   data: "",
+   success: function (data) {
+     arrayReturn = data.data;
+   },
+  });
+}
+
+$(document).on('keypress',function(e) {
+  if(event.key === 'b'){
+   if ($('#ModalComprar').hasClass("show")) {
+     $('#ModalComprar').modal("hide");
+   }else{
+     if (category == null) {
+       $.each(arrayReturn, function() {
+            category = arrayReturn[number].category.split("::");
+            if (category[1] != "Melee") {
+              $("#"+category[1]).append('<div class="'+category[1]+' mt-1"> <img src="'+arrayReturn[number].shopData.newImage+'" alt="" class="img-fluid"> <div>500</div> <div>'+arrayReturn[number].displayName+'</div> </div>');
+            }
+            number++;
+       });
+     }
+     $('#ModalComprar').modal('show');
+   }
+  }
+
 });
