0d432db296b0883a19517c08d30c373f98963388
Eduardo de Oliveira Henrique <dudu.sji06@gmail.com>
2022-06-24 11:28:59 -0300

    Css atualizado pro Julião

--- script/script.js
+++ script/script.js
@@ -1,33 +1,43 @@
 var arrayReturn = {};
 var number = 0;
 var category = null;
-if (arrayReturn != " ") {
-  $.ajax({
-   type: 'GET',
-   url: "https://valorant-api.com/v1/weapons",
-   data: "",
-   success: function (data) {
-     arrayReturn = data.data;
-   },
-  });
-}
-
-$(document).on('keypress',function(e) {
-  if(event.key === 'b'){
-   if ($('#ModalComprar').hasClass("show")) {
-     $('#ModalComprar').modal("hide");
-   }else{
-     if (category == null) {
+$(document).ready(function() {
+  if (arrayReturn != " ") {
+    $.ajax({
+     type: 'GET',
+     url: "https://valorant-api.com/v1/weapons",
+     data: "",
+     success: function (data) {
+       arrayReturn = data.data;
        $.each(arrayReturn, function() {
             category = arrayReturn[number].category.split("::");
             if (category[1] != "Melee") {
-              $("#"+category[1]).append('<div class="'+category[1]+' mt-1"> <img src="'+arrayReturn[number].shopData.newImage+'" alt="" class="img-fluid"> <div>500</div> <div>'+arrayReturn[number].displayName+'</div> </div>');
+              $("#"+category[1]).append('<div class="'+category[1]+' mt-1 gun" data-arma="'+arrayReturn[number].displayName+'"><div class="text-center"> <img src="'+arrayReturn[number].shopData.newImage+'" alt="" class="img-gun"></div> <div>'+arrayReturn[number].shopData.cost+'</div> <div>'+arrayReturn[number].displayName+'</div> </div>');
             }
             number++;
        });
-     }
+       number = 0;
+     },
+    });
+  }
+});
+
+
+$(document).on('keypress',function(e) {
+  if(event.key === 'b' || event.key === 'B'){
+   if ($('#ModalComprar').hasClass("show")) {
+     $('#ModalComprar').modal("hide");
+   }else{
      $('#ModalComprar').modal('show');
    }
   }
+  $( ".gun" ).hover(
+  function() {
 
+    console.log(arrayReturn.find(arrayReturn => arrayReturn.displayName === $(this).data("arma")))
+    $( this ).addClass( "hover" );
+  }, function() {
+    $( this ).removeClass( "hover" );
+  }
+);
 });
